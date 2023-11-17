f5f0b27d74fff0d1c75940b735f2af043c032aa0
+ <+>
2023-11-18 01:46:35 +0300

    completed assignment

--- src/components/ColorList.js
+++ src/components/ColorList.js
@@ -9,18 +9,32 @@ function ColorList() {
     "hotpink",
   ];
 
+  // return (
+  //   <div>
+  //     <h1>Top 5 CSS Colors</h1>
+  //     <ol>
+  //       <li style={{ color: colors[0] }}>{colors[0]}</li>
+  //       <li style={{ color: colors[1] }}>{colors[1]}</li>
+  //       <li style={{ color: colors[2] }}>{colors[2]}</li>
+  //       <li style={{ color: colors[3] }}>{colors[3]}</li>
+  //       <li style={{ color: colors[4] }}>{colors[4]}</li>
+  //     </ol>
+  //   </div>
+  // );
+
+  const colorElements = colors.map((color) => {
+    return <li key={colors.id} style={{ color: color }}>{color}</li>;
+  });
   return (
     <div>
       <h1>Top 5 CSS Colors</h1>
       <ol>
-        <li style={{ color: colors[0] }}>{colors[0]}</li>
-        <li style={{ color: colors[1] }}>{colors[1]}</li>
-        <li style={{ color: colors[2] }}>{colors[2]}</li>
-        <li style={{ color: colors[3] }}>{colors[3]}</li>
-        <li style={{ color: colors[4] }}>{colors[4]}</li>
+        {/* display the array of <li> elements here! */}
+        {colorElements}
       </ol>
     </div>
   );
+
 }
 
 export default ColorList;
